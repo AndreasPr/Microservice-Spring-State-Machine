e446800937944250c1bb7f8bcdbc5971412ddd91
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2020-11-16 18:15:55 -0500

    Transition Configuration

---
+++ src/test/java/andreasgroup/microservicespringstatemachine/config/StateMachineConfigTest.java
@@ -0,0 +1,38 @@
+package andreasgroup.microservicespringstatemachine.config;
+
+import andreasgroup.microservicespringstatemachine.domain.PaymentEvent;
+import andreasgroup.microservicespringstatemachine.domain.PaymentState;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created on 16/Nov/2020 to microservice-spring-state-machine
+ */
+@SpringBootTest
+class StateMachineConfigTest {
+
+    @Autowired
+    StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+    @Test
+    void testNewStateMachine() {
+        StateMachine<PaymentState, PaymentEvent> sm = factory.getStateMachine(UUID.randomUUID());
+        sm.start();
+        System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTHORIZE);
+        System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
+        System.out.println(sm.getState().toString());
+
+
+    }
+}
