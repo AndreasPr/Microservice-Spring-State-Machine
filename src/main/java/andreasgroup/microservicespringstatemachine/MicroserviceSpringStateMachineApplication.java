2b62fcf6bf64db50e2a84a5590a6608b053764ac
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2020-11-16 17:13:08 -0500

    First commit

---
+++ src/main/java/andreasgroup/microservicespringstatemachine/MicroserviceSpringStateMachineApplication.java
@@ -0,0 +1,13 @@
+package andreasgroup.microservicespringstatemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSpringStateMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSpringStateMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andreasgroup/microservicespringstatemachine/MicroserviceSpringStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package andreasgroup.microservicespringstatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSpringStateMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
