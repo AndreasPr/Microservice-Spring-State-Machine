7153e1ea6f57ec20864b08bbb67d8d990396ca7c
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2020-11-17 11:35:14 -0500

    Sending events to the State Machine

--- src/main/java/andreasgroup/microservicespringstatemachine/services/PaymentServiceImpl.java
+++ src/main/java/andreasgroup/microservicespringstatemachine/services/PaymentServiceImpl.java
@@ -5,6 +5,8 @@ import andreasgroup.microservicespringstatemachine.domain.PaymentEvent;
 import andreasgroup.microservicespringstatemachine.domain.PaymentState;
 import andreasgroup.microservicespringstatemachine.repositories.PaymentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
 import org.springframework.statemachine.support.DefaultStateMachineContext;
@@ -19,6 +21,7 @@ public class PaymentServiceImpl implements PaymentService {
 
     private final PaymentRepository paymentRepository;
     private final StateMachineFactory<PaymentState, PaymentEvent> stateMachineFactory;
+    public static final String PAYMENT_ID_HEADER = "payment_id";
 
     @Override
     public Payment newPayment(Payment payment) {
@@ -28,19 +31,28 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        //We are going to get the state machine, we are going to restore that from the database then we are going to
+        // send an event into that state machine. We are saying that with the PREAUTH method it is going to send an payment
+        // event of pre-authorized to the state machine.
+
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+        sendEvent(paymentId, sm, PaymentEvent.PRE_AUTHORIZE);
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+        sendEvent(paymentId, sm, PaymentEvent.AUTH_APPROVED);
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+        sendEvent(paymentId, sm, PaymentEvent.AUTH_DECLINED);
         return null;
     }
 
@@ -49,7 +61,7 @@ public class PaymentServiceImpl implements PaymentService {
         // of the state machine. We stopped it, we use an accessor to go in and set it to the state that is from the database
         // or a payment from the database. We restart the state machine and then we return it. This is a common method for
         // every method that we are going to use for each one of our methods that is going to work with state machine.
-        
+
         Payment payment = paymentRepository.getOne(paymentId);
         StateMachine<PaymentState, PaymentEvent> sm = stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
         sm.stop();
@@ -61,6 +73,15 @@ public class PaymentServiceImpl implements PaymentService {
         sm.start();
 
         return sm;
+    }
+
+    private void sendEvent(Long paymentId, StateMachine<PaymentState, PaymentEvent> sm, PaymentEvent event){
+        Message msg = MessageBuilder
+                .withPayload(event)
+                .setHeader(PAYMENT_ID_HEADER, paymentId)
+                .build();
+        //Enrich the message to have the paymentId in order to know that the StateMachine to react in the event.
+        sm.sendEvent(msg);
     }
 
 
