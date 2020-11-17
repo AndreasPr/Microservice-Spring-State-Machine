8d4cc5275d651f8063a8e0c9916a3f30ed7a860f
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2020-11-17 11:18:42 -0500

    Initializing State Machine from Database

--- src/main/java/andreasgroup/microservicespringstatemachine/services/PaymentServiceImpl.java
+++ src/main/java/andreasgroup/microservicespringstatemachine/services/PaymentServiceImpl.java
@@ -7,6 +7,7 @@ import andreasgroup.microservicespringstatemachine.repositories.PaymentRepositor
 import lombok.RequiredArgsConstructor;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 /**
@@ -27,16 +28,41 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         return null;
     }
+
+    private StateMachine<PaymentState, PaymentEvent> build(Long paymentId){
+        //We are going out to the database, getting a payment, then we are asking the state machine factory for an instance
+        // of the state machine. We stopped it, we use an accessor to go in and set it to the state that is from the database
+        // or a payment from the database. We restart the state machine and then we return it. This is a common method for
+        // every method that we are going to use for each one of our methods that is going to work with state machine.
+        
+        Payment payment = paymentRepository.getOne(paymentId);
+        StateMachine<PaymentState, PaymentEvent> sm = stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
+        sm.stop();
+
+        sm.getStateMachineAccessor()
+                .doWithAllRegions(sma->{
+                    sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null, null));
+                });
+        sm.start();
+
+        return sm;
+    }
+
+
+
 }
